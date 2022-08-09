9bd6907812fd134e83f4d7277725312535009931
Myesha Spyke <mspyke@pr-a1.kzn.21-school.ru>
2022-08-09 15:43:12 +0300

    fixed const_iterator

--- containers/containers_test/srcs/map/ite_arrow.cpp
+++ containers/containers_test/srcs/map/ite_arrow.cpp
@@ -10,7 +10,7 @@ int		main(void)
 	std::list<T3> lst;
 	unsigned int lst_size = 5;
 	for (unsigned int i = 0; i < lst_size; ++i)
-		lst.push_back(T3(2.5 + i, i + 1));
+		lst.push_back(T3(static_cast<float>(2.5 - i), i + 1));
 
 	TESTED_NAMESPACE::map<T1, T2> mp(lst.begin(), lst.end());
 	TESTED_NAMESPACE::map<T1, T2>::iterator it(mp.begin());
